9d379536910149ab9d5a9fe8883f355fe6c8fa22
ljfpudding2014 <ljf_pudding@hotmail.com>
2014-08-23 11:31:43 +0800

    Update BuildQTApplicationCode.bat

--- BuildQTApplicationCode.bat
+++ BuildQTApplicationCode.bat
@@ -1,9 +1,11 @@
 echo off
-echo Setting up environment for C++ 11 with Minw32 GCC 4.8.1 usage...
+echo Setting up environment for QACC Compile usage...
 set path=
 set include=
 set lib=
-set path=C:\MinGW\bin;C:\MinGW\libexec\gcc\mingw32\4.8.1;%PATH%
+set path=C:\MinGW\bin;C:\MinGW\libexec\gcc\mingw32\4.8.1;E:\QT4.8BuildDir\bin;%PATH%
 set LIBRARY_PATH=C:\MinGW\lib;C:\MinGW\mingw32\lib\gcc\mingw32\4.8.1;
 set C_INCLUDE_PATH=c:\MinGW\include;C:\MinGW\mingw32\lib\gcc\mingw32\4.8.1\include;
 set CPLUS_INCLUDE_PATH=c:\MinGW\include;C:\MinGW\mingw32\lib\gcc\mingw32\4.8.1\include\c++;C:\MinGW\mingw32\lib\gcc\mingw32\4.8.1\include\c++\backward;C:\MinGW\mingw32\lib\gcc\mingw32\4.8.1\include\c++\mingw32;
+set include=E:\QT4.8BuildDir\include;E:\QT4.8BuildDir\bin\qwt\include;
+set lib=E:\QT4.8BuildDir\lib;E:\QT4.8BuildDir\bin\qwt\lib;
