2daabfd2181562b964950b29011f93ca424c6743
ljfpudding <ljf_pudding@sina.com>
2014-10-16 16:53:56 +0800

    UserLogin

---
+++ UserLogin.cpp
@@ -0,0 +1,119 @@
+
+#include "UserLogin.h"
+
+UserLoginDialog::UserLoginDialog(QWidget *parent) :
+    QDialog(parent)//,
+    //ui(new Ui::MainWindow)
+{
+    //ui->setupUi(this);
+	setupUi(this);
+	
+	connect(actionConfirm, SIGNAL(clicked), this, SLOT(ConfirmDialog()));
+	connect(actionCancel, SIGNAL(clicked), this, SLOT(CancelDialog()));
+	
+}
+
+UserLoginDialog::~UserLoginDialog()
+{
+    //delete ui;
+	
+}
+
+void UserLoginDialog::setupUi(QWidget * widget )
+{
+        if (widget->objectName().isEmpty())
+            widget->setObjectName(QString::fromUtf8("UserLoginDialog"));
+        widget->resize(230, 153);
+
+        QIcon icon;
+        icon.addFile(QString::fromUtf8(":/res/images/logoNain.png"), QSize(), QIcon::Normal, QIcon::Off);
+        this->setWindowIcon(icon);
+		
+        gridLayout = new QGridLayout(widget);
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        labelUserName = new QLabel(widget);
+        labelUserName->setObjectName(QString::fromUtf8("labelUserName"));
+		labelUserName->setMinimumSize(QSize(30, 0));
+
+        horizontalLayout->addWidget(labelUserName);
+
+        lineEditUsername = new QLineEdit(widget);
+        lineEditUsername->setObjectName(QString::fromUtf8("lineEditUsername"));
+        lineEditUsername->setInputMask("nnnnnnnnnnnnnnn; ");
+
+        horizontalLayout->addWidget(lineEditUsername);
+
+
+        gridLayout->addLayout(horizontalLayout, 0, 0, 1, 1);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        labelPassword = new QLabel(widget);
+        labelPassword->setObjectName(QString::fromUtf8("labelPassword"));
+		labelPassword->setMinimumSize(QSize(30, 0));
+
+        horizontalLayout_2->addWidget(labelPassword);
+
+        lineEditPassword = new QLineEdit(widget);
+        lineEditPassword->setObjectName(QString::fromUtf8("lineEditPassword"));
+
+        horizontalLayout_2->addWidget(lineEditPassword);
+
+
+        gridLayout->addLayout(horizontalLayout_2, 1, 0, 1, 1);
+
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
+        toolButtonConfirm = new QToolButton(widget);
+        toolButtonConfirm->setObjectName(QString::fromUtf8("toolButtonConfirm"));
+        toolButtonConfirm->setMinimumSize(QSize(80, 30));
+
+        horizontalLayout_3->addWidget(toolButtonConfirm);
+
+        toolButtonCancel = new QToolButton(widget);
+        toolButtonCancel->setObjectName(QString::fromUtf8("toolButtonCancel"));
+        toolButtonCancel->setMinimumSize(QSize(80, 30));
+
+        horizontalLayout_3->addWidget(toolButtonCancel);
+
+
+        gridLayout->addLayout(horizontalLayout_3, 2, 0, 1, 1);
+
+
+		actionConfirm = new QAction(this);
+		actionCancel = new QAction(this);
+		
+        retranslateUi(this);
+
+        QMetaObject::connectSlotsByName(this);
+}
+
+void UserLoginDialog::retranslateUi(QDialog *Dialog)
+{
+
+        //Dialog->setWindowTitle(QApplication::translate("UserLoginDialog", "UserLoginDialog", 0, QApplication::UnicodeUTF8));
+        Dialog->setWindowTitle(tr("UserLoginDialog"));
+		//labelUserName->setText(QApplication::translate("UserLoginDialog", "UserName", 0, QApplication::UnicodeUTF8));
+        labelUserName->setText(tr("UserName"));
+		//labelPassword->setText(QApplication::translate("UserLoginDialog", "Password", 0, QApplication::UnicodeUTF8));
+        labelPassword->setText(tr("UserPassword"));
+		//toolButtonConfirm->setText(QApplication::translate("UserLoginDialog", "Confirm", 0, QApplication::UnicodeUTF8));
+        toolButtonConfirm->setText(tr("Confirm"));
+		//toolButtonCancel->setText(QApplication::translate("UserLoginDialog", "Cancel", 0, QApplication::UnicodeUTF8));
+		toolButtonCancel->setText(tr("Cancel"));
+
+}
+
+void UserLoginDialog::ConfirmDialog()
+{
+    
+	done(0);
+}
+
+void UserLoginDialog::CancelDialog()
+{
+
+
+}
---
+++ UserLogin.h
@@ -0,0 +1,58 @@
+#ifndef _H_USERLOGIN
+#define _H_USERLOGIN
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QDialog>
+#include <QtGui/QGridLayout>
+#include <QtGui/QHeaderView>
+#include <QtGui/QLabel>
+#include <QtGui/QLineEdit>
+#include <QtGui/QListView>
+#include <QtGui/QRadioButton>
+#include <QtGui/QSpacerItem>
+#include <QtGui/QSpinBox>
+#include <QtGui/QToolButton>
+#include <QtGui/QWidget>
+#include <QDialog>
+
+class UserLoginDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit UserLoginDialog(QWidget *parent = 0);
+    ~UserLoginDialog();
+	
+	
+public:
+	void setupUi(QWidget * widget );	
+	void retranslateUi(QDialog *Dialog);
+	
+public:
+    QGridLayout *gridLayout;
+    QHBoxLayout *horizontalLayout;
+    QLabel *labelUserName;
+    QLineEdit *lineEditUsername;
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *labelPassword;
+    QLineEdit *lineEditPassword;
+    QHBoxLayout *horizontalLayout_3;
+    QToolButton *toolButtonConfirm;
+    QToolButton *toolButtonCancel;
+
+	QAction *actionConfirm;
+	QAction *actionCancel;
+	
+private slots:
+
+	void ConfirmDialog();
+	void CancelDialog();
+
+	
+};
+
+
+
+#endif
